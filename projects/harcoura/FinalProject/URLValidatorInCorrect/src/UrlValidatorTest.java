239ba1573b7982f85e081b74f2f73a67ac6db24e
Adeline Harcourt <adelineharcourt@gmail.com>
2017-11-30 20:08:02 -0500

    Added Unit Test and formatting

--- projects/harcoura/FinalProject/URLValidatorInCorrect/src/UrlValidatorTest.java
+++ projects/harcoura/FinalProject/URLValidatorInCorrect/src/UrlValidatorTest.java
@@ -38,7 +38,7 @@ public class UrlValidatorTest extends TestCase {
    
    public void testManualTest()
    {
-	   System.out.println("Begining Manual Test: ");
+	   System.out.println("\n>>>>>>>>>>>>>>>>>>> Manual Test <<<<<<<<<<<<<<<<<<\n");
 	   
 	   String[] testURLs = {
 			   ("http://www.facebook.com"), 
@@ -94,9 +94,9 @@ public class UrlValidatorTest extends TestCase {
 		   System.out.println("Test URL: " + testURLs[i]);
 		   System.out.println("Expected Result: " + expectedResults[i]+ "  Actual Result: " + urlVal.isValid(testURLs[i]));
 	   }
+
 	   
-	   
-	   //System.out.println(urlVal.isValid("http://0.0.0.0:80/test1?action=view"));
+	   System.out.println("\n>>>>>>>>>>>>>>> Manual Test Finished <<<<<<<<<<<<<<<");
 	   
 	   
    }
@@ -294,24 +294,211 @@ public class UrlValidatorTest extends TestCase {
 	   System.out.println("\n>>>>>>>> Testing Finished with " + failedTests + "/" + numTests + " Failures <<<<<<<<");
    }
    
-   public void testIsValid()
-   {
-	   for(int i = 0; i <10000; i++)
-	   {
-		   
-	   }
-   }
-   
-   public void testAnyOtherUnitTest()
-   {
-	   
-   }
-   /**
-    * Create set of tests by taking the testUrlXXX arrays and
-    * running through all possible permutations of their combinations.
-    *
-    * @param testObjects Used to create a url.
-    */
-   
+   public void testIsValid(){  
+	 System.out.println("\n>>>>>>>>>>>>>>>>>>>> Unit Test <<<<<<<<<<<<<<<<<<<\n");
+	 int bugCount   = 0,    // counter to keep track of the bugs
+	 loopCount1 = 100,  // counter for the first loop
+	 loopCount2 = 0;    // counter for the second loop
+	
+	 //using this container to hold all of the failed URLs that should have passed
+	 String[] urlFail = new String[loopCount1],
+	
+	 //using this container to hold all of the passing URLs that should have failed
+	          urlPass = new String[loopCount1];
+	
+	 //I am seperating the incorrect and correct values for each corresponding segment of a URL
+	
+	 //all of the following values are correct and were derived from testIsValid() in the URLValidator_Correct file.
+	 String[] correctQuery 	   = { 
+	                                "?action=view", 
+			                        "?action=edit&mode=down", 
+			                        "", 
+			                        "?set=true&bob=dylan&you=true" 
+	 						 	  },
+	
+			  correctPort       = { 
+			                        ":80", 
+			                        ":90", 
+			                        ":100", 
+			                        ":3000", 
+			                        ":8888" 
+			                      },
+			
+			  correctOption     = { 
+			                        "/test1", 
+			                        "/t123", 
+			                        "/$23", 
+			                        "/test/", 
+			                        "/$23/file" 
+			                      },
+			
+			  correctScheme     = { 
+			                        "http://", 
+			                        "", 
+			                        "ftp://", 
+			                        "h3t://" 
+			                      },
+			
+			  correctPath       = { 
+			                        "/test", 
+			                        "/t123", 
+			                        "/$23", 
+			                        "/test/", 
+			                        "/test/next" 
+			                      },
+			
+			  correctAuthority  = { 
+			                        "www.google.com", 
+			                        "google.com", 
+			                        "go.com", 
+			                        "go.au", 
+			                        "0.0.0.0", 
+			                        "255.255.255.255", 
+			                        "255.com", 
+			                        "go.cc" 
+			                              },
+			 
+			 //all of the following values are incorrect and were derived from testIsValid() in the URLValidator_Correct file.
+			  incorrectScheme = { 
+			                      "3ht://",
+			                      "http:", 
+			                      "http:/", 
+			                      "://", 
+			                      "http/" 
+			                    },
+			
+			  incorrectPath   = { 
+			                      "/..", 
+			                      "/../", 
+			                      "/correctPath/to//file", 
+			                      "//#" 
+			                    },
+			
+			  incorrectPort   = { 
+			                      ":-1", 
+			                      ":8888a", 
+			                      ":70000", 
+			                      "22000" 
+			                    },
+			
+			  incorrectAuth   = { 
+			                      "www.google.1com", 
+			                      "256.256.256.256", 
+			                      "16.16.16.16.16", 
+			                      "55.55.55", 
+			                      ".255.255.255.255", 
+			                      "192.68.102.10.", 
+			                      "abc", 
+			                      "abc.123" 
+			                    },
+			
+			  incorrectOpt    = { 
+			                      "/../", 
+			                      "/#", 
+			                      "/test//file" 
+			                    };
+			
+	 int schemePosition, authPosition, portPosition, pathPosition, optionPosition, queryPosition;
 
+	 for(int i = 0; i < loopCount1; i++) {
+	      //Here I am randomizing the position of each segment of protocol using Math.random, and I typecast the result to an int to get rid of the decimal
+	      optionPosition  = (int) (Math.floor(Math.random() * 5));
+	      portPosition    = (int) (Math.floor(Math.random() * 5));
+	      authPosition    = (int) (Math.floor(Math.random() * 8));
+	      pathPosition    = (int) (Math.floor(Math.random() * 5));
+	      queryPosition   = (int) (Math.floor(Math.random() * 4));
+	      schemePosition  = (int) (Math.floor(Math.random() * 4));
+
+	      //Parsing together a string using the valid options
+	      String corrURL = correctScheme[schemePosition] + correctAuthority[authPosition] + correctPort[portPosition] + correctPath[pathPosition] + correctQuery[queryPosition];
+
+	      UrlValidator legit =  new UrlValidator();
+	      //passing in the string we made above to isValid. This should return a 0 for false or a 1 for true!
+	      boolean verify = legit.isValid(corrURL);
+
+	      //since we saved the result to a boolean, we can directly check if the string was counted as valid or invalid
+	      if ( verify == false ){
+	    	  urlFail[loopCount2] = corrURL; //saving the failed URL in our container
+	    	  loopCount2++; //incrementing the counter to reflect the index of out urlFail array.
+	    	  bugCount++; //since it is false, we count this as a bug and add it to the counter!
+	      }
+
+	 }
+
+	 //Let the user know how many bugs were present
+	 System.out.println("Bugs Present in valid URL testing: " + bugCount);
+	 
+	 System.out.println("\nBugged URLs:\n");
+	
+	 for ( int i=0; i< urlFail.length; i++){
+		 if ( urlFail[i] != null ) System.out.println( "\n" + i + "- " + urlFail[i]);
+	 }
+	
+	 System.out.println("\nSuccessful completion of testing valid URLs.\n");
+	
+	 //Reset our counters in order to test invalid URLs
+	 bugCount   = 0;
+	 loopCount2 = 0;
+	
+	 //making a string made of all valid components except for a single invalid component
+	 for(int i = 0; i < loopCount1; i++) {
+	       //picking a value in the corresponding array at random
+	       pathPosition   = (int) (Math.floor(Math.random() * 5));
+	       schemePosition = (int) (Math.floor(Math.random() * 4));
+	       portPosition   = (int) (Math.floor(Math.random() * 5));
+	       authPosition   = (int) (Math.floor(Math.random() * 8));
+	       optionPosition = (int) (Math.floor(Math.random() * 5));
+	       queryPosition  = (int) (Math.floor(Math.random() * 4));
+
+
+	       //assigning index positions to the invalid components
+	       int invalidSchemePos = (int) (Math.floor(Math.random() * 5)),
+	           invalidOptPos    = (int) (Math.floor(Math.random() * 3)),
+	           invalidPathPos   = (int) (Math.floor(Math.random() * 4)),
+	           invalidAuthPos   = (int) (Math.floor(Math.random() * 8)),
+	           invalidPortPos   = (int) (Math.floor(Math.random() * 4)),
+	           invalidQueryPos  = (int) (Math.floor(Math.random() * 4));
+
+	       //determine which component will be replaced
+	       int switchIn = (int) (Math.floor(Math.random() * 4));
+
+	       //Create a random string made up of valid URL pieces and one invalid piece
+	       String corrURL = null;
+	       if( switchIn == 0 ){  
+	         //replace scheme
+	         corrURL = incorrectScheme[invalidSchemePos] + correctAuthority[authPosition] + correctPort[portPosition] + correctPath[pathPosition] + correctQuery[queryPosition];
+	       } else if( switchIn == 1 ){ 
+	         //replace auth
+	         corrURL = correctScheme[schemePosition] + incorrectAuth[invalidAuthPos] + correctPort[portPosition] + correctPath[pathPosition] + correctQuery[queryPosition];
+	       } else if( switchIn == 2){ 
+	         //replace port
+	         corrURL = correctScheme[schemePosition] + correctAuthority[authPosition] + incorrectPort[invalidPortPos] + correctPath[pathPosition] + correctQuery[queryPosition];
+	       } else if( switchIn == 3){ 
+	         //replace path
+	         corrURL = correctScheme[schemePosition] + correctAuthority[authPosition] + correctPort[portPosition] + incorrectPath[invalidPathPos] + correctQuery[queryPosition];
+	       }
+	       //please note that since no incorrect queries were provided in the original URL Validator Test file I have left them out here as well.
+
+	       UrlValidator isLegit =  new UrlValidator();
+	       //passing in the string we made above and checking to see what happens next
+	       boolean verify = isLegit.isValid(corrURL);
+
+	       if ( verify == true ) {
+	             bugCount++;  //since verify should not be true, we count it as a bug         
+	             urlPass[loopCount2] = corrURL; //store the bugged URL in our container
+	             loopCount2++;  //moving to the next index in our URL array           
+	           }
+	   }
+
+	   //show user the results
+	   System.out.println("\nInvalid URLs that are considered valid:" + bugCount);
+	   System.out.println("\nHere are the Invalid URLs that UrlValidator considered to be valid: \n");
+	   for ( int i=0; i< urlFail.length; i++) {
+		   if ( urlPass[i] != null ) System.out.println( "\n" + i + "- " + urlPass[i]);
+	   }
+	   
+	   System.out.println("\n>>>>>>>>>>>>>>>> Unit Test Finished <<<<<<<<<<<<<<<<");
+
+   }
+   
 }
